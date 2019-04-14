e135ba7d7896863de594ffd567df284c424412b8
thoughtexpress <ankitpachauri3@gmail.com>
2019-04-14 07:50:44 +0000

    First Commit

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -1,42 +1,42 @@
 #ifndef FusionEKF_H_
 #define FusionEKF_H_
 
-#include <fstream>
-#include <string>
-#include <vector>
-#include "Eigen/Dense"
-#include "kalman_filter.h"
 #include "measurement_package.h"
+#include "Eigen/Dense"
+#include <vector>
+#include <string>
+#include <fstream>
+#include "kalman_filter.h"
 #include "tools.h"
 
 class FusionEKF {
- public:
+public:
   /**
-   * Constructor.
-   */
+  * Constructor.
+  */
   FusionEKF();
 
   /**
-   * Destructor.
-   */
+  * Destructor.
+  */
   virtual ~FusionEKF();
 
   /**
-   * Run the whole flow of the Kalman Filter from here.
-   */
+  * Run the whole flow of the Kalman Filter from here.
+  */
   void ProcessMeasurement(const MeasurementPackage &measurement_pack);
 
   /**
-   * Kalman Filter update and prediction math lives in here.
-   */
+  * Kalman Filter update and prediction math lives in here.
+  */
   KalmanFilter ekf_;
 
- private:
-  // check whether the tracking toolbox was initialized or not (first measurement)
+private:
+  // check whether the tracking toolbox was initiallized or not (first measurement)
   bool is_initialized_;
 
   // previous timestamp
-  long long previous_timestamp_;
+  long previous_timestamp_;
 
   // tool object used to compute Jacobian and RMSE
   Tools tools;
@@ -46,4 +46,4 @@ class FusionEKF {
   Eigen::MatrixXd Hj_;
 };
 
-#endif // FusionEKF_H_
+#endif /* FusionEKF_H_ */
